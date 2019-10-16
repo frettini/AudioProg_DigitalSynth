f9174daca51f3b62d2e55a4a1fe1f7e2cad63683
Nicolae <nicolae.marton@gmail.com>
2019-10-16 15:22:35 +0100

    made it work boyo

--- lab3/modules/osc.py
+++ lab3/modules/osc.py
@@ -2,20 +2,16 @@ import numpy as np
 import math
 from scipy import signal
 
-
-class Generator(object):
-    def gen_buffer(self, buffer):
-        pass
-
 class WaveGen(object):
     
     def __init__(self, sample_rate = 44100):
         #default sample sate is 44100
 
-        self.__frequency = 0
-        self.__amplitude = 0
-        self.__phase = 0
-        self.__sample_rate = sample_rate
+        self._frequency = 0
+        self._amplitude = 0
+        self._phase = 0
+        self._sample_rate = sample_rate
+        
 
     
     def cal_phase(self):
--- lab3/modules/sine_oscillator.py
+++ lab3/modules/sine_oscillator.py
@@ -1,44 +1,46 @@
-import osc
+from .osc import WaveGen
 import numpy as np
 import math
 
-class SineOsc(osc.WaveGen):
+class SineOsc(WaveGen):
 
     def __init__(self, sample_rate = 44100):
-        super().__init__(sample_rate)
+        
         self.states = np.zeros(2)
+        super().__init__(sample_rate)
+        print(self._phase)
 
 
     #properties setters and getters---------------------------------
     @property
     def frequency(self):
         print("get freq")
-        return self.__frequency
+        return self._frequency
 
     @frequency.setter
     def frequency(self, value):
         print("set freq")
-        if value >= 0 or value <= self.__sample_rate/2:
-            self.__frequency = value
-        elif value > self.__sample_rate/2:
-            self.__frequency = self.__sample_rate/2
+        if value >= 0 or value <= self._sample_rate/2:
+            self._frequency = value
+        elif value > self._sample_rate/2:
+            self._frequency = self._sample_rate/2
         else:
-            self.__frequency = 0
+            self._frequency = 0
 
     @property
     def amplitude(self):
         print("get amp")
-        return self.__amplitude
+        return self._amplitude
 
     @amplitude.setter
     def amplitude(self, value):
         print("set amp")
         if value >= 0 or value <= 1:
-            self.__amplitude = value
+            self._amplitude = value
         elif value > 1:
-            self.__amplitude = 1
+            self._amplitude = 1
         else:
-            self.__amplitude = 0
+            self._amplitude = 0
     
     #-------------------------------------------------------------
 
@@ -56,23 +58,19 @@ class SineOsc(osc.WaveGen):
 
         if self.states[1] > self.states[0]:
             #going up
-            print("going up boi")
-            self.__phase = phase
+            self._phase = phase
         elif self.states[1] < self.states[0] :
             #going down
-            print("going down bois")
-            self.__phase = np.pi - phase
+            self._phase = np.pi - phase
 
 
-    def gen_buffer(self, buffer):
+    def gen_buffer(self, buffer_size):
         #takes in a buffer of any size in the form of nparray
+        self.buffer = np.arange(0,buffer_size)
+        self._phase = self._phase % (2*math.pi)
 
-        self.buffer = np.arange(0,len(buffer))
-        self.__phase = self.__phase % (2*math.pi)
-
-        x = ((self.buffer * 2*math.pi * self.__frequency)/self.sample_rate) % (2*math.pi)
-        result = self.__amplitude * np.sin(x + self.__phase)
-        
+        x = ((self.buffer * 2*math.pi * self._frequency)/self._sample_rate) % (2*math.pi)
+        result = self._amplitude * np.sin(x + self._phase)
         result = result/np.max(result)
 
         self.states = result[-2:]
