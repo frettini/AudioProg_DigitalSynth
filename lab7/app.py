7c7ba40ce33d07cdb1f704b66dea208b7171c534
Nicolae <nicolae.marton@gmail.com>
2019-11-27 16:43:17 +0000

    woop woop

--- lab7/app.py
+++ lab7/app.py
@@ -10,18 +10,21 @@ d = delay.Delay(10, 2)
 g = delay.Gain(2)
 f = filter.Filter(0.99, 0.1425, 1., 0.0)
 
-f.a1 = 0
-f.a2 = -1
-f.b1 = -1.7553711
-f.b2 = 0.9025
+# f.a1 = 0
+# f.a2 = -1
+# f.b1 = -1.7553711
+# f.b2 = 0.9025
+
+#use scipy.signal.iirfilter to get coefficients "sos" mode
 
 buffer1 = np.zeros(2048)
 
-og1 = osc.gen_buffer(2048, end_freq = 15000)
-
-result = f.filtering(og1)
+# og1 = osc.gen_buffer(2048, end_freq = 15000)
+white_buffer = white.gen_buffer(204800)
+result = f.filtering(white_buffer)
+# white_freq = scipy.fftpack.fft(og1)
 result_freq = scipy.fftpack.fft(result)
 
 
-plot.plot(np.imag(result_freq))
+plot.plot(np.real(result_freq))
 plot.show()
